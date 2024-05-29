c4c26675541a161a2b2497ba4ad824ca4930efae
shivkumarojha <ojhashivkumar88@gmail.com>
2024-05-29 15:32:23 +0530

    renamed some file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,12 @@ import 'dotenv/config'
 const app = express()
 
 // admin router
-import adminRouter from "./routes/admin.routes"
+import adminRouter from "./routes/adminRoute"
 app.use('/api/v1/admin', adminRouter)
 
 
 // Student Router
-import studentRouter from "./routes/students.routes"
+import studentRouter from "./routes/studentRoute"
 app.use("/api/v1/student", studentRouter)
 
 app.listen(process.env.PORT, () => {
