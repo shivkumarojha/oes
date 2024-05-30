d74c41574e82f0a96f27a582c98d4f0aaf00817d
shivkumarojha <ojhashivkumar88@gmail.com>
2024-05-30 16:00:50 +0530

    update admin schema added

--- backend/src/schemas/admin.schema.ts
+++ backend/src/schemas/admin.schema.ts
@@ -22,3 +22,11 @@ export const AdminLoginSchema = AdminSchema.pick({email: true, password: true}).
 export type AdminLoginType = z.infer<typeof AdminLoginSchema>
 export type AdminType = z.infer<typeof AdminSchema>
 
+// Update admin Schema
+export const UpdateAdminSchema = z.object({
+    name: z.string({ message: "Name is required" }),
+    userName: z.string({ message: "User must be lowercase " }).toLowerCase(),
+    profilePic: z.string().url({ message: "Profile pic must be an url" }).optional()
+})
+
+export type UpdateAdminSchema = z.infer<typeof UpdateAdminSchema>
